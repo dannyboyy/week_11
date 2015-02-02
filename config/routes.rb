6094c1a38cc2efd1cbbf3540ce07c9e669422c29
Danny Wu <wushuo1988@hotmail.com>
2015-02-02 00:06:22 -0500

    Change root based on if user signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
+  resources :notes
+
+  # If user signed in, show the notes index view, otherwise show the welcome index view
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root"
+  end
 
   root 'welcome#index'
-
-  resources :notes
 end
